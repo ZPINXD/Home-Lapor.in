310a1b302c623df48adaa9d9c426b3061e7d86ec
ZPINXD <geovanogalan@gmail.com>
2025-03-09 09:07:53 +0700

    Modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,22 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0c770c",
+        secondary: "#fbbf24",
+        back: "#f0fdf4",
+      },
+      animation: {
+        fade: "fade 1.5s ease-in-out",
+      },
+      keyframes: {
+        fade: {
+          "0%": { opacity: "0.4" },
+          "100%": { opacity: "1" },
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
